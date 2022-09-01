953679dd0ac932b6d05fe022dd8d319d43fb54f5
Wojciech Kokoszka <wojciech.kokoszka94@gmail.com>
2022-09-01 21:47:03 +0200

    Created simple stock data dashboard

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,30 @@
+let dataDisplays = document.querySelectorAll('.data-item__display')
+let symbolsQuery = document.querySelectorAll('.data-item__display__symbol')
+let pricesQuery = document.querySelectorAll('.data-item__display__price')
+
 const baseQuery = 'https://www.alphavantage.co/query?function='
 const globalQuoteFun = 'GLOBAL_QUOTE'
-const symbolQuote = '&symbol=IBM'
-const apiKey = '&apikey=demo'
+const symbolQuote = '&symbol='
+const apiKey = '&apikey=XGPBHNB9IWTJ80FR'
 
 const endPoint = baseQuery + globalQuoteFun + symbolQuote + apiKey
 
-JSON.
+let data1
 
-console.log('Endpoint:' + endPoint)
+const loadData = (symbol, id) => {
+	const query = baseQuery + globalQuoteFun + symbolQuote + symbol + apiKey
+	fetch(query)
+		.then(response => response.json())
+		.then(data => {
+			data1 = data
+			symbolsQuery[id].textContent = data1['Global Quote']['01. symbol']
+			pricesQuery[id].textContent = data1['Global Quote']['05. price']
+		})
+}
+
+loadData('IBM', 0)
+loadData('AAPL', 1)
+loadData('ABB', 2)
+loadData('GOOGL', 3)
+loadData('MSFT', 4)
+loadData('NFLX', 5)
