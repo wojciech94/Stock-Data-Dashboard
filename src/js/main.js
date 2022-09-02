bc3d60ed5e284ad27f304e1cb8f9c688bec5d8b4
Wojciech Kokoszka <wojciech.kokoszka94@gmail.com>
2022-09-02 18:53:59 +0200

    Improved page theme colors, Added panel percentage change

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 let dataDisplays = document.querySelectorAll('.data-item__display')
-let symbolsQuery = document.querySelectorAll('.data-item__display__symbol')
-let pricesQuery = document.querySelectorAll('.data-item__display__price')
+let symbolsText = document.querySelectorAll('.data-item__display__symbol')
+let pricesText = document.querySelectorAll('.data-item__display__price')
+let changesText = document.querySelectorAll('.data-item__display__change')
 
 const baseQuery = 'https://www.alphavantage.co/query?function='
 const globalQuoteFun = 'GLOBAL_QUOTE'
@@ -11,23 +12,43 @@ const endPoint = baseQuery + globalQuoteFun + symbolQuote + apiKey
 
 let symbols
 let prices
+let changes
+let data1
 
 const loadData = (symbol, id) => {
-	if (localStorage.getItem('Price' + id) != null) {
-		symbolsQuery[id].textContent = localStorage.getItem('Symbol' + id)
-		pricesQuery[id].textContent = localStorage.getItem('Price' + id)
+	if (localStorage.getItem('Change' + id) != null) {
+		symbolsText[id].textContent = localStorage.getItem('Symbol' + id)
+		pricesText[id].textContent = localStorage.getItem('Price' + id)
+		let changePercent = Number(localStorage.getItem('Change' + id).replace('%', ''))
+		setChangeColor(changePercent, id)
+		changesText[id].textContent = localStorage.getItem('Change' + id)
 		console.log('Loaded from local Storage')
 	} else {
 		const query = baseQuery + globalQuoteFun + symbolQuote + symbol + apiKey
 		fetch(query)
 			.then(response => response.json())
 			.then(data => {
-				let data1 = data
-				symbolsQuery[id].textContent = data1['Global Quote']['01. symbol']
-				pricesQuery[id].textContent = data1['Global Quote']['05. price']
+				data1 = data
+				symbolsText[id].textContent = data1['Global Quote']['01. symbol']
+				pricesText[id].textContent = data1['Global Quote']['05. price']
+				let changePercent = Number(data1['Global Quote']['10. change percent'].replace('%', ''))
+				setChangeColor(changePercent, id)
+				changesText[id].textContent = changePercent + '%'
 				localStorage.setItem('Symbol' + id, data1['Global Quote']['01. symbol'])
 				localStorage.setItem('Price' + id, data1['Global Quote']['05. price'])
+				localStorage.setItem('Change' + id, data1['Global Quote']['10. change percent'])
 			})
+	}
+}
+
+function setChangeColor(num, id) {
+	console.log('Num:' + num)
+	if (num > 0) {
+		changesText[id].classList.add('data-item__display__change--up')
+		changesText[id].classList.remove('data-item__display__change--down')
+	} else {
+		changesText[id].classList.add('data-item__display__change--down')
+		changesText[id].classList.remove('data-item__display__change--up')
 	}
 }
 
